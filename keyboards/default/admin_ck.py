e7e74cac3b07b0a1d39627666c4bd76664edf877
Bunyod-Abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2023-12-22 19:30:45 +0500

    Update admin_ck.py

--- keyboards/default/admin_ck.py
+++ keyboards/default/admin_ck.py
@@ -3,5 +3,10 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 async def admin_get_url(url):
     key = ReplyKeyboardMarkup(resize_keyboard=True)
-    key.row("Login", WebAppInfo(url=f'{url}'))
+    key.insert(KeyboardButton(
+        text="Login", web_app=WebAppInfo(
+            url=f'{url}'
+        )
+    )
+    )
     return key
